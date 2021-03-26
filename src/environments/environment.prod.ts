a84016cb496e3f968f9425b575294c0182444f54
Ales Sedlacek <xsedla1c@vutbr.cz>
2021-03-26 22:51:44 +0100

    added production environment properties

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBbwSKYjaUWFVdg5rFw1u4jCFCtIswHcsQ',
+    authDomain: 'participatory-budgeting-295014.firebaseapp.com',
+    databaseURL: 'https://participatory-budgeting-295014.firebaseio.com',
+    projectId: 'participatory-budgeting-295014',
+    storageBucket: 'participatory-budgeting-295014.appspot.com',
+    messagingSenderId: '216903213229',
+    appId: '1:216903213229:web:834d95392cc79f5aba6624',
+    measurementId: 'G-63SELFMJLM',
+  },
 };
